6122c7fca7236157cb5e6d92994493bd707258b9
Mohammad Alauddin Emon <95302046+Emon5419@users.noreply.github.com>
2022-03-01 19:17:55 +0600

    details add

--- js/app.js
+++ js/app.js
@@ -1,47 +1,57 @@
 document.getElementById('error-message').style.display = 'none';
 const searchPhone = () => {
-    const searchField = document.getElementById('search-field');
-    const searchText = searchField.value;
-    // clear data
-    searchField.value = '';
-    document.getElementById('error-message').style.display = 'none';
+  const searchField = document.getElementById('search-field');
+  const searchText = searchField.value;
 
-    // load data
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-    // console.log(url);
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displaySearchResult(data.data))
-        .catch(error => displayError(error));
+  // clear data
+  searchField.value = '';
+  document.getElementById('error-message').style.display = 'none';
 
+  // load data
+  const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+  fetch(url)
+    .then(res => res.json())
+    .then(data => displaySearchResult(data.data))
+    .catch(error => displayError(error));
 }
-
 const displayError = error => {
-    document.getElementById('error-message').style.display = 'block';
+  document.getElementById('error-message').style.display = 'block';
 }
-
 const displaySearchResult = phones => {
-    const searchResult = document.getElementById('search-result');
-    searchResult.textContent = '';
-
-
-    phones.forEach(phone => {
-
-        // console.log(phone);
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
+  const searchResult = document.getElementById('search-result');
+  searchResult.textContent = '';
+  phones.forEach(phone => {
+    const div = document.createElement('div');
+    div.classList.add('col');
+    div.innerHTML = `
         <div class="cards1 card mx-5 btn btn-outline-light">
             <img src="${phone.image}" class="img card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${phone.phone_name}</h5>
-                
             </div>
-            <button onclick="loadMealDetail(${phone.phoneid})" type="button" class="btn btn-outline-primary w-25 m-auto">Primary</button>
+            <button onclick="loadPhoneDetail('${phone.phoneid}')" type="button" class="btn btn-outline-primary w-25 m-auto">Details</button>
         </div>
         `;
-        searchResult.appendChild(div);
-
-
-    })
+    searchResult.appendChild(div);
+  })
+}
+const loadPhoneDetail = phoneid => {
+  const url = `https://openapi.programming-hero.com/api/phone/${phoneid}`;
+  fetch(url)
+    .then(res => res.json())
+    .then(data => displayPhoneDetail(data));
+}
+const displayPhoneDetail = phone => {
+  const phoneDetails = document.getElementById('phone-details');
+  const div = document.createElement('div');
+  div.classList.add('card');
+  div.innerHTML = `
+    <img src="${phone.phone_name}" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">${phone.name}</h5>
+        <p class="card-text">${phone.data.mainFeatures}</p>
+        <a href="${phone.displaySize}" class="btn btn-primary">Go somewhere</a>
+    </div>
+    `;
+  phoneDetails.appendChild(div);
 }
