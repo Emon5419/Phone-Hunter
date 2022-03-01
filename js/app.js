fb390b047fc4e7c64dbbd4aff0ac13feda4b1cb5
Mohammad Alauddin Emon <95302046+Emon5419@users.noreply.github.com>
2022-03-01 14:52:34 +0600

    error handling and load first api

--- js/app.js
+++ js/app.js
@@ -1,8 +1,21 @@
-
+document.getElementById('error-message').style.display = 'none';
 const searchPhone = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     // clear data
     searchField.value = '';
-    console.log(searchText);
+    document.getElementById('error-message').style.display = 'none';
+
+    // load data
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    // console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displaySearchResult(data.data))
+        .catch(error => displayError(error));
+
+}
+
+const displayError = error => {
+    document.getElementById('error-message').style.display = 'block';
 }
