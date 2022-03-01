7e3e09efc1fa684310b8bc204cea2b1300193d4b
Mohammad Alauddin Emon <95302046+Emon5419@users.noreply.github.com>
2022-03-01 15:21:03 +0600

    search result and showign card

--- js/app.js
+++ js/app.js
@@ -19,3 +19,29 @@ const searchPhone = () => {
 const displayError = error => {
     document.getElementById('error-message').style.display = 'block';
 }
+
+const displaySearchResult = phones => {
+    const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
+
+
+    phones.forEach(phone => {
+
+        // console.log(phone);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="cards1 card mx-5 btn btn-outline-light">
+            <img src="${phone.image}" class="img card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${phone.phone_name}</h5>
+                
+            </div>
+            <button onclick="loadMealDetail(${phone.phoneid})" type="button" class="btn btn-outline-primary w-25 m-auto">Primary</button>
+        </div>
+        `;
+        searchResult.appendChild(div);
+
+
+    })
+}
