1db449e0ca2b7f4d1c3133fd5faffc9039d69210
Mohammad Alauddin Emon <95302046+Emon5419@users.noreply.github.com>
2022-03-02 16:48:34 +0600

    BuyNow-Button Add

--- js/app.js
+++ js/app.js
@@ -37,7 +37,7 @@ const displaySearchResult = phones => {
     searchResult.appendChild(div);
   })
 }
-
+//load details api:
 const loadPhoneDetail = phoneid => {
   console.log(phoneid);
   const url = `https://openapi.programming-hero.com/api/phone/${phoneid}`;
@@ -53,47 +53,41 @@ const displayPhoneDetail = phones => {
   div.classList.add('card');
   div.innerHTML = `
   <div class="cards1">
-    
-  <div class="row g-0">
-        <img src="${phones.data.image}" class="img-fluid rounded-start" alt="...">
+    <div class="row g-0">
+          <img src="${phones.data.image}" class="img-fluid rounded-start" alt="...">
+        <div class="col-md-8">
+        <div class="card-body">
+          <h2 class="card-title">${phones.data.name}</h2>
+          <p class="card-title">Name: ${phones.data.releaseDate}</p>
 
-    <div class="col-md-8">
+          //Main Features Section:
+          <h5 class="card-title mt-5">Main Features:</h5>
+          <p class="card-title">Storage: ${phones.data.mainFeatures.storage}</p>
+          <p class="card-text">Chipset: ${phones.data.mainFeatures.chipSet}</p>
+          <p class="card-text">DisplaySize: ${phones.data.mainFeatures.displaySize}</p>
+          <p class="card-text">Memory: ${phones.data.mainFeatures.memory}</p>
 
-      <div class="card-body">
-        <h2 class="card-title">${phones.data.name}</h2>
-        <p class="card-title">Name: ${phones.data.releaseDate}</p>
-
-        //Main Features Section:
-        <h5 class="card-title mt-5">Main Features:</h5>
-        <p class="card-title">Storage: ${phones.data.mainFeatures.storage}</p>
-        <p class="card-text">Chipset: ${phones.data.mainFeatures.chipSet}</p>
-        <p class="card-text">DisplaySize: ${phones.data.mainFeatures.displaySize}</p>
-        <p class="card-text">Memory: ${phones.data.mainFeatures.memory}</p>
-
-        //Sensors Section:
-        <h5 class="card-text mt-2">Sensors:</h5>
-        <p class="card-text">1: ${phones.data.mainFeatures.sensors[0]}</p>
-        <p class="card-text">2: ${phones.data.mainFeatures.sensors[1]}</p>
-        <p class="card-text">3: ${phones.data.mainFeatures.sensors[2]}</p>
-        <p class="card-text">4: ${phones.data.mainFeatures.sensors[3]}</p>
-        <p class="card-text">5: ${phones.data.mainFeatures.sensors[4]}</p>
-        <p class="card-text">6: ${phones.data.mainFeatures.sensors[5]}</p>
-        
-        // Others Section:
-        <h5 class="card-text mt-2">Others:</h5>
-        <p class="card-text">Bluetooth:{phones.data.others.Bluetooth}</p>
-        <p class="card-text">GPS:${phones.data.others.GPS}</p>
-        <p class="card-text">NFC:${phones.data.others.NFC}</p>
-        <p class="card-text">Radio:${phones.data.others.Radio}</p>
-        <p class="card-text">USB:${phones.data.others.USB}</p>
-        <p class="card-text">WLAN:${phones.data.others.WLAN}</p>
-
-
+          //Sensors Section:
+          <h5 class="card-text mt-2">Sensors:</h5>
+          <p class="card-text">1: ${phones.data.mainFeatures.sensors[0]}</p>
+          <p class="card-text">2: ${phones.data.mainFeatures.sensors[1]}</p>
+          <p class="card-text">3: ${phones.data.mainFeatures.sensors[2]}</p>
+          <p class="card-text">4: ${phones.data.mainFeatures.sensors[3]}</p>
+          <p class="card-text">5: ${phones.data.mainFeatures.sensors[4]}</p>
+          <p class="card-text">6: ${phones.data.mainFeatures.sensors[5]}</p>
+          
+          // Others Section:
+          <h5 class="card-text mt-2">Others:</h5>
+          <p class="card-text">Bluetooth:{phones.data.others.Bluetooth}</p>
+          <p class="card-text">GPS:${phones.data.others.GPS}</p>
+          <p class="card-text">NFC:${phones.data.others.NFC}</p>
+          <p class="card-text">Radio:${phones.data.others.Radio}</p>
+          <p class="card-text">USB:${phones.data.others.USB}</p>
+          <p class="card-text">WLAN:${phones.data.others.WLAN}</p>
+          <button type="button" class="btn btn-outline-primary"> Buy Now </button>
+        </div>
       </div>
     </div>
-  </div>
-
-</div>
-    `;
+  </div>`;
   phoneDetails.appendChild(div);
 }
